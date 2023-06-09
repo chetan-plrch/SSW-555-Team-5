3b34fc87d6b65bdac5401341a5e20e163aeb3f27
Chetan Jain <chetanplrch@gmail.com>
2023-06-08 21:28:54 -0400

    feature: update the code with the latest files

--- parse.py
+++ parse.py
@@ -1,53 +1,171 @@
-import sys
+import datetime
+from dateutil.relativedelta import relativedelta
+from prettytable import PrettyTable
 
-def is_tag_valid(tag):
-    return tag in ['INDI', 'NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'FAM', 'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV', 'DATE', 'HEAD', 'TRLR', 'NOTE']
+x = PrettyTable()
+y = PrettyTable()
 
-def is_exception_tag(level, arguments):
-    return level == 0 and (arguments in ['INDI', 'FAM'])
+supported_tags = {'INDI': '0', 'NAME': '1', 'SEX': '1', 'BIRT': '1', 'DEAT': '1', 'FAMC': '1', 'FAMS': '1', 'FAM': '0',
+              'MARR': '1', 'HUSB': '1', 'WIFE': '1', 'CHIL': '1', 'DIV': '1', 'DATE': '2', 'HEAD': '0',
+              'TRLR': '0', 'NOTE': '0'}
 
 def is_not_supported_tags(level, tag):
     return (level == 1 and tag == 'DATE') or (level == 2 and tag == 'NAME')
 
-def understand_line(line):
-    line = line.rstrip('\n')
-    output = []
 
-    parts = line.split(' ')
-    level = int(parts[0])
-    tag = parts[1]
-    arguments = ' '.join(parts[2:])
+def clean_gedcom_tags(file_path, supported_tags):
+    clean_tags = []
 
-    output.append('--> ' + line)
+    with open(file_path) as file:
+        raw_gedcom = file.read().splitlines()
 
-    valid = 'N'
-    if is_exception_tag(level, arguments):
-        if is_tag_valid(arguments): valid = 'Y'
-    else:
-        if is_tag_valid(tag): valid = 'Y'
+    for tag in raw_gedcom:
+        tag_parts = tag.strip().split(" ", 2)
 
-    if is_not_supported_tags(level, tag):
-        pass
-    elif is_exception_tag(level, arguments):
-        output.append('<-- {}|{}|{}|{}'.format(level, arguments, valid, tag))
-    else:
-        output.append('<-- {}|{}|{}|{}'.format(level, tag, valid, arguments))
+        if len(tag_parts) < 2:
+            continue
 
-    write_to_output(output)
+        if len(tag_parts) > 2 and tag_parts[1] in ['INDI', 'FAM']:
+            continue
 
-def write_to_output(lines):
-    f = open("output.txt", "a")
-    for line in lines:
-        f.write(f"{line}\n")
-    f.close()
+        if len(tag_parts) > 2 and tag_parts[2] in ['INDI', 'FAM']:
+            tag_parts[1], tag_parts[2] = tag_parts[2], tag_parts[1]
 
-def read_file():
-    with open(sys.argv[1], 'r') as file:
-        for line in file:
-            understand_line(line)
+        if len(tag_parts) > 2:
+            if tag_parts[1] in supported_tags and supported_tags[tag_parts[1]] == tag_parts[0]:
+                clean_tags.append((tag_parts[0], tag_parts[1], tag_parts[2]))
+            elif tag_parts[0:2] == ["In", ""]:
+                clean_tags.append(tag_parts)
 
-# Instructions on how to run the file:
-# 1. Please navigate the project folder
-# 2. Clean the output.txt file
-# 3. Please use the command "python3 parse.py sample.ged" to process the file "sample.ged" and generate output in "output.txt" file
-read_file()
+        elif len(tag_parts) == 2:
+            if tag_parts[1] in supported_tags and supported_tags[tag_parts[1]] == tag_parts[0]:
+                clean_tags.append((tag_parts[0], tag_parts[1]))
+
+    clean_tags = clean_tags[1:-1]
+    clean_tags = list(filter(('1', 'BIRT').__ne__, clean_tags))
+
+    return clean_tags
+
+def collect_individual_metadata(individuals, clean_tags):
+    x.field_names = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse']
+
+    for i, tag in enumerate(clean_tags):
+        if tag[1] == 'INDI' and tag[0] == '0':
+            indi_key = tag[2][1:-1]
+            values = []
+            j = i + 1
+            skip_tag = 0
+
+            while clean_tags[j][1] != 'INDI':
+                if clean_tags[j][1] == 'FAM' and clean_tags[j][0] == '0':
+                    skip_tag = 1
+                    break
+                elif clean_tags[j][1] == 'DEAT' and clean_tags[j][2] == 'Y':
+                    values.append(('DEAT', clean_tags[j + 1][2]))
+                    j += 1
+                elif clean_tags[j][1] in ('FAMS', 'FAMC'):
+                    values.append((clean_tags[j][1], clean_tags[j][2][1:-1]))
+                else:
+                    values.append((clean_tags[j][1], clean_tags[j][2]))
+                j += 1
+
+            individuals[indi_key] = values
+
+            if skip_tag == 1:
+                break
+
+    name, gender, birth_date, death_date = "", "", "", ""
+    alive = False
+    for key, value in individuals.items():
+        for i in range(len(value)):
+            family_child, family_spouse = "", ""
+            tag = value[i][0]
+            if tag == 'NAME':
+                name = value[i][1]
+            if tag == 'SEX':
+                gender = value[i][1]
+            if tag == 'DATE':
+                birth_date = value[i][1]
+                birth_date = datetime.datetime.strptime(birth_date, '%d %b %Y').date()
+            if tag == 'DEAT':
+                death_date = value[i][1]
+                death_date = datetime.datetime.strptime(death_date, '%d %b %Y').date()
+            if tag == 'FAMC':
+                family_child = value[i][1]
+            if tag == 'FAMS':
+                family_spouse = value[i][1]
+        if any('DEAT' in i for i in value):
+            alive = True
+            age = relativedelta(death_date, birth_date).years
+        else:
+            age = relativedelta(datetime.datetime.now(), birth_date).years
+
+        x.add_row([key, name, gender, birth_date, age, alive, death_date, family_child, family_spouse])
+
+    print("Individuals")
+    print(x)
+    x.clear_rows()
+
+
+def collect_family_metadata(individuals, families, clean_tags):
+    y = PrettyTable()
+
+    for i in range(len(clean_tags)):
+        values = []
+        j = i + 1
+        if clean_tags[i][1] == 'FAM' and clean_tags[i][0] == '0':
+            family_key = clean_tags[i][2][1:-1]
+
+            while j < len(clean_tags):
+                tag = clean_tags[j][1]
+                data = clean_tags[j][2][1:-1] if len(clean_tags[j]) > 2 else ""
+
+                if tag not in ['MARR', 'DIV', 'DATE', 'FAM']:
+                    values.append((tag, data))
+                elif tag == 'MARR' and len(clean_tags[j + 1]) > 2:
+                    values.append(('MARR', clean_tags[j + 1][2]))
+                elif tag == 'DIV' and len(clean_tags[j + 1]) > 2:
+                    values.append(('DIV', clean_tags[j + 1][2]))
+                elif tag == 'FAM' and clean_tags[j][0] == '0':
+                    break
+
+                j += 1
+
+            families[family_key] = values
+
+    y.field_names = ['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children']
+
+    for key, value in families.items():
+        children = []
+        married_date, divorce_date = "", ""
+        husband_id, wife_id = "", ""
+        husband_name, wife_name = "", ""
+
+        for tag, data in value:
+            if tag == 'HUSB':
+                husband_id = data
+                husband_name = individuals[husband_id][0][1]
+            elif tag == 'WIFE':
+                wife_id = data
+                wife_name = individuals[wife_id][0][1]
+            elif tag == 'CHIL':
+                children.append(data)
+            elif tag == 'MARR':
+                married_date = datetime.datetime.strptime(data, '%d %b %Y').date()
+            elif tag == 'DIV':
+                divorce_date = datetime.datetime.strptime(data, '%d %b %Y').date()
+
+        y.add_row([key, married_date, divorce_date, husband_id, husband_name, wife_id, wife_name, children])
+
+    print("Families")
+    print(y)
+    y.clear_rows()
+
+def parse_gedcom_file():
+    individuals = {}
+    families = {}
+    clean_tags = clean_gedcom_tags('sample.ged', supported_tags)
+    collect_individual_metadata(individuals, clean_tags)
+    collect_family_metadata(individuals, families, clean_tags)
+
+parse_gedcom_file()
