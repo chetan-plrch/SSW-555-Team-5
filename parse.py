3f5a2b9b1d0b0f661b34e6eacd9a8b98392ee481
Rachel Y <40032955+Reiamulette@users.noreply.github.com>
2023-06-15 20:00:56 -0400

    US06-US08
    User Story 06 & User Story 08 Code Update & Test Case added

--- parse.py
+++ parse.py
@@ -244,20 +244,22 @@ def check_if_married_before_death(marriage_date, death_date):
             return err
 
 
-# Story Id - US06
-def divorce_before_death(families, individuals):
+def get_individual_name(indi_id, individuals):
+    return individuals[indi_id]["NAME"]
+
+def check_divorce_before_death(families, individuals):
     count = 0
     for id in families:
         if "DIV" in families[id]:
-            div_date = datetime.strptime(families[id]["DIV"], "%d %b %Y")
+            div_date = datetime.datetime.strptime(families[id]["DIV"], "%d %b %Y")
             print(div_date)
             husb_ID = families[id]["HUSB"]
             wife_ID = families[id]["WIFE"]
             if "DEAT" in individuals[husb_ID] and "DEAT" in individuals[wife_ID]:
-                husbDeath = datetime.strptime(
+                husbDeath = datetime.datetime.strptime(
                     individuals[husb_ID]["DEATH_DATE"], "%d %b %Y"
                 )
-                wifeDeath = datetime.strptime(
+                wifeDeath = datetime.datetime.strptime(
                     individuals[wife_ID]["DEATH_DATE"], "%d %b %Y"
                 )
                 if div_date > husbDeath and div_date > wifeDeath:
@@ -271,7 +273,7 @@ def divorce_before_death(families, individuals):
                         + "' cannot occur after both partners' deaths."
                     )
             elif "DEAT" in individuals[husb_ID]:
-                husbDeath = datetime.strptime(
+                husbDeath = datetime.datetime.strptime(
                     individuals[husb_ID]["DEATH_DATE"], "%d %b %Y"
                 )
                 if div_date > husbDeath:
@@ -286,60 +288,50 @@ def divorce_before_death(families, individuals):
                         + get_individual_name(husb_ID, individuals).replace("/", "")
                         + "'s death."
                     )
-                elif "DEAT" in individuals[wife_ID]:
-                    wifeDeath = datetime.strptime(
-                        individuals[wife_ID]["DEATH_DATE"], "%d %b %Y")
+            elif "DEAT" in individuals[wife_ID]:
+                wifeDeath = datetime.datetime.strptime(
+                    individuals[wife_ID]["DEATH_DATE"], "%d %b %Y")
                 if div_date > wifeDeath:
                     count += 1
                     print(
                         "ERROR: FAMILY: US06: Divorce between "
-                        + "'"
-                        + get_individual_name(husb_ID, individuals).replace("/", "")
-                        + "' and '"
-                        + get_individual_name(wife_ID, individuals).replace("/", "")
-                        + "' cannot occur after "
-                        + get_individual_name(wife_ID, individuals).replace("/", "")
-                        + "'s death."
-                    )
+                    + "'"
+                    + get_individual_name(husb_ID, individuals).replace("/", "")
+                    + "' and '"
+                    + get_individual_name(wife_ID, individuals).replace("/", "")
+                    + "' cannot occur after "
+                    + get_individual_name(wife_ID, individuals).replace("/", "")
+                    + "'s death."
+                )
     if count == 0:
         return True
     else:
         return False
-#print("US06: Divorce before Death" + str(divorce_before_death(y,x)))
+print("US06:" + str(check_divorce_before_death(y,x)))
 
-# Story Id - US08
 def check_birth_after_parent_marriage(families, individuals):
     is_valid = True
     for id in families:
-        if "MARR" in families[id]:
-            # Check for edge case of MARR not containing date
-            if "DATE" in families[id]:
-                marriageDate = datetime.strptime(
-                    families[id]["DATE"], "%d %b %Y"
-                ).date()
-                for childID in families[id]["CHIL"]:
-                    childBirthDate = datetime.strptime(
-                        individuals[childID]["DATE"], "%d %b %Y"
-                    ).date()
-                    if childBirthDate < marriageDate:
-                        print(
-                            "FAMILY: US08: Child ("
+        if "MARR" in families[id] or "DATE" in families[id]:
+            marriageDate = datetime.datetime.strptime(families[id]["MARR"], "%d %b %Y").date()
+            for childID in families[id]["CHIL"]:
+                childBirthDate = datetime.datetime.strptime(
+                    individuals[childID]["DATE"], "%d %b %Y").date()
+                if childBirthDate < marriageDate:
+                    print("ANOMALY: FAMILY: US08: Child ("
                             + get_individual_name(childID, individuals).replace("/", "")
                             + ") born before marriage of parents in family: "
                             + id
-                            + "."
-                        )
-                        is_valid = False
-            else:
-                print(
-                    "ERROR: FILE: US08: Marriage date not set or properly formatted, Check "
-                    + id
-                    + "."
-                )
-                is_valid = False
+                            + ".")
+                    is_valid = False
+        else:
+            print("ERROR: FILE: US08: Marriage date not set or properly formatted of family: "
+                + id
+                + ".")
+            is_valid = False
     return is_valid
 
-#print("US08: Birth After Parents Marriage:" + str(check_birth_after_parent_marriage(y,x)))
+print("US08:" + str(check_birth_after_parent_marriage(y,x)))
 
 def parse_gedcom_file(file_name):
     individuals = {}
---
+++ test_US06_US08py.py
@@ -0,0 +1,218 @@
+import pytest
+
+from parse import *
+
+# US06: Divorce before death
+
+def test_US06_1():
+    families = {"@F1@": {"HUSB": "@I1@", "WIFE": "@I2@","DIV": "30 APR 2000"}}
+    individuals = {
+        "@I1@": {
+        "NAME": "Arthur /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "10 OCT 1974",
+        "FAMS": "@F1@",
+        "FAMC": "@F3@",
+    },
+        "@I2@": {
+        "NAME": "Amanda /Yu/",
+        "SEX": "F",
+        "BIRT": "",
+        "DATE": "29 OCT 1976",
+        "DEAT": "Y",
+        "DEATH_DATE": "26 NOV 2000",
+        "FAMS": "@F1@",
+        "FAMC": "@F4@",
+    }}
+    assert check_divorce_before_death(families, individuals) == True
+    print('Divorce is before Wife death date')
+
+def test_US06_2():
+    families = {"@F1@": {"HUSB": "@I1@", "WIFE": "@I2@","DIV": "30 APR 2018"}}
+    individuals = {
+        "@I1@": {
+        "NAME": "Arthur /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "10 OCT 1974",
+        "DEAT": "Y",
+        "DEATH_DATE": "26 NOV 2020",
+        "FAMS": "@F1@",
+        "FAMC": "@F3@",
+    },
+        "@I2@": {
+        "NAME": "Amanda /Yu/",
+        "SEX": "F",
+        "BIRT": "",
+        "DATE": "29 OCT 1976",
+        "FAMS": "@F1@",
+        "FAMC": "@F4@",
+    }}
+    assert check_divorce_before_death(families, individuals) == True
+    print('Divorce is before Husband death date')
+
+
+def test_US06_3():
+    families = {"@F1@": {"HUSB": "@I1@", "WIFE": "@I2@","DIV": "30 APR 2018"}}
+    individuals = {
+        "@I1@": {
+        "NAME": "Arthur /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "10 OCT 1974",
+        "DEAT": "Y",
+        "DEATH_DATE": "26 NOV 2000",
+        "FAMS": "@F1@",
+        "FAMC": "@F3@",
+    },
+        "@I2@": {
+        "NAME": "Amanda /Yu/",
+        "SEX": "F",
+        "BIRT": "",
+        "DATE": "29 OCT 1976",
+        "FAMS": "@F1@",
+        "FAMC": "@F4@",
+    }}
+    assert check_divorce_before_death(families, individuals) == False
+    print('Divorce is after Husband death date')
+
+def test_US06_4():
+    families = {"@F1@": {"HUSB": "@I1@", "WIFE": "@I2@"}}
+    individuals = {
+        "@I1@": {
+        "NAME": "Arthur /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "10 OCT 1974",
+        "FAMS": "@F1@",
+        "FAMC": "@F3@",
+    },    
+        "@I2@": {
+        "NAME": "Amanda /Yu/",
+        "SEX": "F",
+        "BIRT": "",
+        "DATE": "29 OCT 1976",
+        "DEAT": "Y",
+        "DEATH_DATE": "26 NOV 2000",
+        "FAMS": "@F1@",
+        "FAMC": "@F4@",
+    }}
+    assert check_divorce_before_death(families, individuals) == True
+    print('No divorce date found')
+
+def test_US06_5():
+    families = {"@F1@": {"HUSB": "@I1@", "WIFE": "@I2@","DIV": "30 APR 2018"}}
+    individuals = {
+        "@I1@": {
+        "NAME": "Arthur /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "10 OCT 1974",
+        "FAMS": "@F1@",
+        "FAMC": "@F3@",
+    },
+        "@I2@": {
+        "NAME": "Amanda /Yu/",
+        "SEX": "F",
+        "BIRT": "",
+        "DATE": "29 OCT 1976",
+        "DEAT": "Y",
+        "DEATH_DATE": "26 NOV 2000",
+        "FAMS": "@F1@",
+        "FAMC": "@F4@",
+    }}
+    assert check_divorce_before_death(families, individuals) == False
+    print('Divorce is after Wife death date')
+
+# US08: Birth After Parents Marriage
+
+def test_US08_1():
+    families = {"@F1@": 
+        {"HUSB": "@I1@", 
+         "WIFE": "@I2@",
+         "CHIL": ["@I3@"],
+         "MARR": "30 APR 1997",
+         "DIV": "30 APR 2018"
+        }}
+    individuals = {
+        "@I3@": {
+        "NAME": "Lawrence /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "5 JAN 1998",
+        "FAMC": "@F1@"
+    }}
+    assert check_birth_after_parent_marriage(families, individuals) == True
+    print('Birth is after Parent Marriage')
+
+def test_US08_2():
+    families = {"@F1@": 
+        {"HUSB": "@I1@", 
+         "WIFE": "@I2@",
+         "CHIL": ["@I3@"],
+         "MARR": "30 APR 2000",
+        }}
+    individuals = {
+        "@I3@": {
+        "NAME": "Lawrence /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "5 JAN 1998",
+        "FAMC": "@F1@"
+    }}
+    assert check_birth_after_parent_marriage(families, individuals) == False
+    print('Birth is before Parent Marriage')
+
+def test_US08_3():
+    families = {"@F1@": 
+        {"HUSB": "@I1@", 
+         "WIFE": "@I2@",
+         "CHIL": ["@I3@"],
+        }}
+    individuals = {
+        "@I3@": {
+        "NAME": "Lawrence /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "5 JAN 1998",
+        "FAMC": "@F1@"
+    }}
+    assert check_birth_after_parent_marriage(families, individuals) == False
+    print('No Marriage Date')
+
+def test_US08_4():
+    families = {"@F1@": 
+        {"HUSB": "@I1@", 
+         "WIFE": "@I2@",
+         "CHIL": ["@I3@"],
+         "MARR": "6 JAN 2000"
+        }}
+    individuals = {
+        "@I3@": {
+        "NAME": "Lawrence /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "5 JAN 2000",
+        "FAMC": "@F1@"
+    }}
+    assert check_birth_after_parent_marriage(families, individuals) == False
+    print('Marriage After Birthday')
+
+def test_US08_5():
+    families = {"@F1@": 
+        {"HUSB": "@I1@", 
+         "WIFE": "@I2@",
+         "CHIL": ["@I3@"],
+         "MARR": "4 JAN 2000"
+        }}
+    individuals = {
+        "@I3@": {
+        "NAME": "Lawrence /Li/",
+        "SEX": "M",
+        "BIRT": "",
+        "DATE": "5 JAN 2000",
+        "FAMC": "@F1@"
+    }}
+    assert check_birth_after_parent_marriage(families, individuals) == True
+    print('Marriage After Birthday')
