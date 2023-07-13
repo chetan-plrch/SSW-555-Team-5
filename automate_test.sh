1dda49a458dd1c122fcd340dcb461f15c9df47d5
Chetan Jain <chetanplrch@gmail.com>
2023-07-13 14:06:39 -0400

    change script

--- automate_test.sh
+++ automate_test.sh
@@ -3,8 +3,6 @@
 for f in test*.py; do
     output=$(python3 "$f" | tr '[:upper:]' '[:lower:]')
 
-    if [[ $output ]]; then
-        echo "Error: Test case failed in $f"
-        exit 1
-    fi
+    echo "Error: Test case failed in $f"
+    exit 1
 done
